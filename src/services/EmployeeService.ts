81ac040034958ec53747d4730b4d565a25622472
jericoatos <jericoatos@gmail.com>
2024-08-15 16:36:09 +0100

    Fixed linting errors

--- src/services/EmployeeService.ts
+++ src/services/EmployeeService.ts
@@ -2,7 +2,7 @@ import axios, {AxiosResponse } from "axios";
 import { EmployeeResponse } from "../models/EmployeeResponse";
 import { getHeader } from "./AuthUtil";
 
-export const getSalesEmployee = async (token: String): Promise<EmployeeResponse[]> => {
+export const getSalesEmployee = async (token: string): Promise<EmployeeResponse[]> => {
 
     try{
         const response: AxiosResponse = await axios.get("http://localhost:8080/api/employees/sales", getHeader(token));
@@ -15,7 +15,7 @@ export const getSalesEmployee = async (token: String): Promise<EmployeeResponse[
 
 }
 
-export const getDeliveryEmployee = async (token: String): Promise<EmployeeResponse[]> => {
+export const getDeliveryEmployee = async (token: string): Promise<EmployeeResponse[]> => {
 
     try{
         const response: AxiosResponse = await axios.get("http://localhost:8080/api/employees/delivery", getHeader(token));
